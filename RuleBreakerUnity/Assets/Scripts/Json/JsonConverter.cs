3793c343825b33e738e3de0994ae80629df0fad5
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2025-06-27 09:48:13 +0200

    Initial JSON of board state
    Includes change to required packages: com.unity.nuget.newtonsoft-json

---
+++ RuleBreakerUnity/Assets/Scripts/Json/JsonConverter.cs
@@ -0,0 +1,54 @@
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonConverter : MonoBehaviour
+{
+    [Header("This component does not need to be added to a gameobject, but comes with simple test functions.")]
+    public bool testBoardStateToJSON = false;
+
+    [TextArea(10,100)]
+    public string response;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (testBoardStateToJSON)
+        {
+            testBoardStateToJSON = false;
+            response = BoardStateToJSON();
+        }
+    }
+
+    public static string BoardStateToJSON()
+    {
+        Dictionary<string, string> boardState = new Dictionary<string, string>();
+
+        for (char file = 'A'; file <= 'H'; file++)
+        {
+            for (int rank = 1; rank <= 8; rank++)
+            {
+                string coord = $"{file}{rank}";
+
+                if (GameManager.gameManager.p1Coords.Contains(coord))
+                    boardState[coord] = "Player1";
+                else if (GameManager.gameManager.p2Coords.Contains(coord))
+                    boardState[coord] = "Player2";
+                else
+                    boardState[coord] = "Empty";
+            }
+        }
+
+        string json = JsonConvert.SerializeObject(boardState, Formatting.Indented);
+        Debug.Log(json);
+        return json;
+    }
+}
